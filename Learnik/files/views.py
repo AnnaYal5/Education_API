592407b83d4a0afc19ea9a5c9a98ca22f23f4e61
AnnaYal5 <gannam08@gmail.com>
2025-11-26 21:42:23 +0200

    add services

--- Learnik/files/views.py
+++ Learnik/files/views.py
@@ -1,33 +1,27 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from django.shortcuts import get_object_or_404
-from .models import FileLearning
+from .services import get_file_content_service, FileServiceError
 
 
 @api_view(['GET'])
 def get_file_content(request, file_id):
     try:
-        learning_file = get_object_or_404(FileLearning, id=file_id)
-
-        file_path = learning_file.file.path
-        with open(file_path, 'r', encoding='utf-8') as f:
-            content = f.read()
+        title, content = get_file_content_service(file_id)
 
         return Response({
             'file_id': file_id,
-            'title': learning_file.title,
+            'title': title,
             'content': content
         }, status=status.HTTP_200_OK)
 
-    except FileNotFoundError:
+    except FileServiceError as e:
         return Response(
-            {"detail": "Файл не знайдено."},
+            {"detail": str(e)},
             status=status.HTTP_404_NOT_FOUND
         )
-    except Exception as e:
+    except Exception:
         return Response(
-            {"detail": f"Помилка при читанні файлу: {e}"},
+            {"detail": "Невідома внутрішня помилка сервера."},
             status=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
